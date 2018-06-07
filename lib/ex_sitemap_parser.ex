775d94c5bc42ad644222aab92c4778607170c8bd
Mike Benner <refriedchicken@gmail.com>
2018-06-07 14:37:53 -0700

    initial commit

---
+++ lib/ex_sitemap_parser.ex
@@ -0,0 +1,18 @@
+defmodule ExSitemapParser do
+  @moduledoc """
+  Documentation for ExSitemapParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSitemapParser.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_sitemap_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSitemapParserTest do
+  use ExUnit.Case
+  doctest ExSitemapParser
+
+  test "greets the world" do
+    assert ExSitemapParser.hello() == :world
+  end
+end
