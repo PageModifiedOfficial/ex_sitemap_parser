3036bae717856eedd4d63bbb1db2dd9a4190441a
Mike Benner <refriedchicken@gmail.com>
2018-06-11 10:07:35 -0700

    handle success and error

--- lib/ex_sitemap_parser.ex
+++ lib/ex_sitemap_parser.ex
@@ -12,10 +12,21 @@ defmodule ExSitemapParser do
       :world
 
   """
+  def sitemap_for(url) do
+    url |> get_map |> process_response
+  end
+
   def get_map(url) do
     case HTTPoison.get(url) do
-      {:ok, response} -> IO.inspect response
-      {:error, msg} -> IO.inspect msg
+      {:ok, response} -> response
+      {:error, msg} -> msg
     end
+  end
+
+  def process_response(%HTTPoison.Error{} = response) do
+    IO.inspect response, label: "Error"
+  end
+  def process_response(%HTTPoison.Response{} = response) do
+    IO.inspect response, label: "Success"
   end
 end
