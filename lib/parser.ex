f1e68190536752db29c4303224fa6fc9a8b8dadc
Mike Benner <refriedchicken@gmail.com>
2018-06-11 14:28:42 -0700

    parse doc into list of urls

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,7 +1,14 @@
 defmodule ExSitemapParser.Parser do
   import SweetXml
 
+  @schema [
+      urls: [ ~x[//urlset/url/loc]l,
+        url: ~x[./text()]s
+      ]
+    ]
+
   def parse(doc) do
-    SweetXml.parse(doc)
+    #SweetXml.parse(doc)
+    doc |> SweetXml.xmap(@schema)
   end
 end
