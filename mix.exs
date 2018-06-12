14a2f6b6ce2dd22fc470f78b2a93542a535ce8c2
Mike Benner <refriedchicken@gmail.com>
2018-06-12 10:07:20 -0700

    dummy change name

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule ExSitemapParser.MixProject do
   defp package() do
     [
       # This option is only needed when you don't want to use the OTP application name
-      name: "postgrex",
+      name: "ex_sitemap_parser",
       # These are the default files included in the package
       files: ["lib", "mix.exs", "README*", "LICENSE*"],
       licenses: ["MIT"],
